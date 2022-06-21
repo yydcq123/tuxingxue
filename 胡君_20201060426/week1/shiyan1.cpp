8174f582003b3bf158350c7f8287af8d9f43f303
yydcq123 <57498133+yydcq123@users.noreply.github.com>
2022-06-21 22:59:27 +0800

    Create shiyan1.cpp

---
+++ 胡君_20201060426/week1/shiyan1.cpp
@@ -0,0 +1,32 @@
+#include<windows.h>
+#include<GL/glut.h>
+
+void init(void) {
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0.0, 200.0, 0.0, 150.0);
+}
+
+void lineSegment(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(0.0, 0.4, 0.2);
+    glBegin(GL_LINES);
+    glVertex2i(180, 15);
+    glVertex2i(10, 145);
+    glEnd();
+
+    glFlush();
+}
+
+void main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(50, 100);
+    glutCreateWindow("An Example OpenGL Program");
+
+    init();
+    glutDisplayFunc(lineSegment);
+    glutMainLoop();
+}
